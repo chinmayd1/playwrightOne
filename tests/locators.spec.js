0aba9c06a99ea51701b9ff80ebcc834fe5a5e0ac
chinmayd1 <chinmay.minskole@gmail.com>
2026-03-24 21:31:35 +0530

    added

---
+++ tests/locators.spec.js
@@ -0,0 +1,28 @@
+// @ts-check
+import { test, expect } from '@playwright/test'
+
+test('verify header', async ({ page }) => {
+    
+    // tc1 
+    // Arrangement
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    // Actions(CSS selector) 
+    //tagName[attribute=value]
+    //<h1>Practice Page</h1>
+    let headingElement = await page.locator('h1')
+    let text = await headingElement.textContent()
+    // Assertion 
+    console.log(text)
+    await expect(text).toEqual('Practice Page')
+
+    // example
+    // <input id="name" name="enter-name" class="inputs" placeholder="Enter Your Name" type="text">
+    let inputElement = page.locator('#name')
+    let inputElementByClassFirst  = page.locator('.inputs').first()
+    let inputElementByClassLast  = page.locator('.inputs').last()
+    let inputElementByClassSecond  = page.locator('.inputs').nth(2)
+    let inputElementByClassThird  = page.locator('.inputs').nth(3)
+
+    // Playwright Css - xpath
+
+})
