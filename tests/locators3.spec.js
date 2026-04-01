d67e833794a7a89f3f73f5dcde045e02127d1242
chinmayd1 <chinmay.minskole@gmail.com>
2026-04-01 23:17:28 +0530

    added

--- tests/locators3.spec.js
+++ tests/locators3.spec.js
@@ -21,5 +21,9 @@ test.only('verify header', async ({ page }) => {
     // or()
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     await page.locator('input[type="radio"]').first().check()
+    await page.locator('input[type="radio"]').last().check()
+    await page.locator('input[type="radio"]').nth(1).check()
+    await page.locator('#product').first().locator("tr").filter({hasText:" Total Amount Collected: 296 "})
+    await page.locator('#product').first().locator("tr").filter({has:page.locator('td')})
 
 })
