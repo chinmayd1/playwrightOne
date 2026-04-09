78f964a595642469f268729d3668d4e71aa3f390
chinmayd1 <chinmay.minskole@gmail.com>
2026-04-09 21:42:55 +0530

    added

--- tests/locators4.spec.js
+++ tests/locators4.spec.js
@@ -2,7 +2,7 @@
 // Element state methods
 
 // isVisible()
-// isHidden()
+// isHdden()
 // isChecked()
 // isEnabled()
 // isDisabled()
@@ -27,24 +27,55 @@ test.only('Element state methods', async ({ page }) => {
     // radio , checkbox  -  isChecked()
 
 
-    await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
-    let radioButtonOne = await page.locator("input[value='radio1']")
-    let expectedValue3 = await radioButtonOne.isChecked()
-    await expect(expectedValue3).toBeFalsy()
-    await radioButtonOne.click()
-    expectedValue3 = await radioButtonOne.isChecked()
-    await expect(expectedValue3).toBeTruthy()
+    // await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    // let radioButtonOne = await page.locator("input[value='radio1']")
+    // let expectedValue3 = await radioButtonOne.isChecked()
+    // await expect(expectedValue3).toBeFalsy()
+    // await radioButtonOne.click()
+    // expectedValue3 = await radioButtonOne.isChecked()
+    // await expect(expectedValue3).toBeTruthy()
 
 
-    await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
-    let checkBoxOne = await page.locator("input[value='option1']")
-    let expectedValue4 = await checkBoxOne.isChecked()
-    await expect(expectedValue4).toBeFalsy()
-    await checkBoxOne.click()
-    expectedValue4 = await checkBoxOne.isChecked()
-    await expect(expectedValue4).toBeTruthy()
+    // await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    // let checkBoxOne = await page.locator("input[value='option1']")
+    // let expectedValue4 = await checkBoxOne.isChecked()
+    // await expect(expectedValue4).toBeFalsy()
+    // await checkBoxOne.click()
+    // expectedValue4 = await checkBoxOne.isChecked()
+    // await expect(expectedValue4).toBeTruthy()
+
+    // await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    // let buttonOpenWindow = await page.locator('#openwindow')
+    // let result = buttonOpenWindow.isDisabled()
+    // await expect(result).toBeFalsy()
+
+    // await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    // let buttonOpenWindowB = await page.locator('#openwindow')
+    // let resultB = buttonOpenWindowB.isEnabled()
+    // await expect(resultB).toBeTruthy()
+
+  
+    // static waits and dynamic waits 
+    //thread.sleep(2000)
+    // dynamic wait()
+    // explicit and implicit
+    // element  and overall script
+    // explicit --- 5   ---- element ---> 2 
+    // implicit ---5  ----  element ----> 2
+    
+
+    // await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
+    // let hideButton = page.locator('#hide-textbox')  
+    // let showButton = page.locator('#show-textbox')
+    // let  displayTextInput = page.locator('#displayed-text')  
+    // showButton.click()
+    // displayTextInput.waitFor({state:'visible'})
+    // hideButton.click()
+    // displayTextInput.waitFor({state:'hidden'})
 
 
+
+  
 
 
 
---
+++ tests/locators5.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test'
+test.only('Element state methods', async ({ page }) => {
+
+
+    // click()
+    // dbclick()
+    // clear()
+    // fill()
+    // press()
+    // hover()
+    // check()
+    // uncheck()
+    // selectOption()
+    // scrollIntoViewIfNeeded()
+    // screenshot()
+
+
+
+});
