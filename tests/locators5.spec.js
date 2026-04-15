dc150272ef74a319cb23edc5aec069dffd9c8654
chinmayd1 <chinmay.minskole@gmail.com>
2026-04-15 21:38:37 +0530

    added

--- tests/locators5.spec.js
+++ tests/locators5.spec.js
@@ -32,9 +32,44 @@ test.only('Element state methods', async ({ page }) => {
     // await inputEType.type('in')
     
     // press()
-    let inputEType = await page.locator('#autocomplete')
-    await inputEType.fill('Ar')
-    await inputEType.press('ArrowDown')
-    await inputEType.press('Enter')
+    // let inputEType = await page.locator('#autocomplete')
+    // await inputEType.fill('Ar')
+    // await inputEType.press('ArrowDown')
+    // await inputEType.press('Enter')
+
+    // checkbox
+    let checkBoxOne = await page.locator('#checkBoxOption1')
+    let checkBoxTwo = await page.locator('#checkBoxOption2')
+    let checkBoxThree = await page.locator('#checkBoxOption3')
+    //await checkBoxOne.click()
+    await checkBoxOne.check()
+    await checkBoxTwo.check()
+    await checkBoxThree.check()
+    let expectedValue = await checkBoxOne.isChecked()
+    await expect(expectedValue).toBeTruthy()
+
+    await checkBoxOne.uncheck()
+    let expectedValue2 = await checkBoxOne.isChecked()
+    await expect(expectedValue2).toBeFalsy()
+    await checkBoxTwo.uncheck()
+    await checkBoxThree.uncheck()
+    
+    // radio button
+    let radioButtonOne = await page.locator("input[value='radio1']")
+    let radioButtonTwo = await page.locator("input[value='radio2']")
+    await radioButtonOne.check()
+    let expectedValue3 = await checkBoxOne.isChecked()
+    await expect(expectedValue3).toBeTruthy()
+    let expectedValue4 = await radioButtonTwo.isChecked()
+    await expect(expectedValue4).toBeFalsy()
+    
+    // drop down 
+    let selectDropDown = await page.locator('#dropdown-class-example')
+    await selectDropDown.selectOption('option1'); // value / index / Text
+    
+    let hoverElement = await page.locator('#mousehover')
+    hoverElement.hover()
+
+    
 
 });
