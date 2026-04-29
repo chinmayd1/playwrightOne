9d8b5ce8cf6357935bd307b95cfcd5665e8939da
chinmayd1 <chinmay.minskole@gmail.com>
2026-04-29 22:14:02 +0530

    added

--- tests/locators8.spec.js
+++ tests/locators8.spec.js
@@ -23,6 +23,63 @@ test.only('Element state methods', async ({ page }) => {
     let e = await page.locator('#name').evaluate((el) => el.tagName)
     console.log(e)
 
+    // type = "text"
+    // type = "number"
+    // <input type = number/>
+
+    // program 1
+
+    let type = await page.locator('#name').evaluate((el)=>el.type)
+    console.log(type)
+    let placeholder = await page.locator('#name').evaluate((el)=>el.placeholder)
+    console.log(placeholder)
+    let id = await page.locator('#name').evaluate((el)=>el.id)
+    console.log(id)
+
+    // program 2
+
+     let q2 = await page.locator('#name').evaluate((el) => ({
+        type: el.type,
+        id: el.id,
+        placeholder:el.placeholder
+    }));
+    console.log(q2);
+
+    // program 3
+
+    let q22 = await page.locator('input[type="checkbox"]').evaluateAll((els)=>els.map(function(el,index,arr){
+        return el.id
+    }))
+    console.log(q22)
+
+   // radio buttons
+
+   let q23 = await page.locator('#product').first().locator('tr').first().locator('th').evaluateAll(els=>els.map((el)=>el.textContent))
+    console.log(q23)
+
+
+
+
+
+    // let a = [11,22,33]
+    // let q11 = a.map(function(ell,index,arr){
+    //     return ell + 10
+    // })
+    // console.log(a)
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
 
 
 
