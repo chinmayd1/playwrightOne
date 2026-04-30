15ed86566b5a01131d787e0eb59ec67867ff926b
chinmayd1 <chinmay.minskole@gmail.com>
2026-04-30 21:32:27 +0530

    added

--- tests/locators7.spec.js
+++ tests/locators7.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-test.only('Element state methods', async ({ page }) => {
+test('Element state methods', async ({ page }) => {
 
     // Element info methods 
     
--- tests/locators8.spec.js
+++ tests/locators8.spec.js
@@ -20,8 +20,8 @@ test.only('Element state methods', async ({ page }) => {
 
     //evalute()
 
-    let e = await page.locator('#name').evaluate((el) => el.tagName)
-    console.log(e)
+    // let e = await page.locator('#name').evaluate((el) => el.tagName)
+    // console.log(e)
 
     // type = "text"
     // type = "number"
@@ -29,43 +29,112 @@ test.only('Element state methods', async ({ page }) => {
 
     // program 1
 
-    let type = await page.locator('#name').evaluate((el)=>el.type)
-    console.log(type)
-    let placeholder = await page.locator('#name').evaluate((el)=>el.placeholder)
-    console.log(placeholder)
-    let id = await page.locator('#name').evaluate((el)=>el.id)
-    console.log(id)
+    // let type = await page.locator('#name').evaluate((el)=>el.type)
+    // console.log(type)
+    // let placeholder = await page.locator('#name').evaluate((el)=>el.placeholder)
+    // console.log(placeholder)
+    // let id = await page.locator('#name').evaluate((el)=>el.id)
+    // console.log(id)
 
     // program 2
-
-     let q2 = await page.locator('#name').evaluate((el) => ({
-        type: el.type,
-        id: el.id,
-        placeholder:el.placeholder
-    }));
-    console.log(q2);
+    // single elements attribue
+    //  let q2 = await page.locator('#name').evaluate((el) => ({
+    //     type: el.type,
+    //     id: el.id,
+    //     placeholder:el.placeholder
+    // }));
+    // console.log(q2);
 
     // program 3
+    // ids
+    // let q22 = await page.locator('input[type="checkbox"]')
+    // .evaluateAll((els)=>els.map(function(el,index,arr){
+    //     return el.id
+    // }))
+    // console.log(q22)
 
-    let q22 = await page.locator('input[type="checkbox"]').evaluateAll((els)=>els.map(function(el,index,arr){
-        return el.id
-    }))
-    console.log(q22)
-
-   // radio buttons
-
-   let q23 = await page.locator('#product').first().locator('tr').first().locator('th').evaluateAll(els=>els.map((el)=>el.textContent))
-    console.log(q23)
-
-
-
-
+   // table -- th text
+//    let q23 = await page.locator('#product')
+//    .first()
+//    .locator('tr').first()
+//    .locator('th')
+//    .evaluateAll(els=>els.map((el)=>el.textContent))
+//     console.log(q23)
 
     // let a = [11,22,33]
     // let q11 = a.map(function(ell,index,arr){
     //     return ell + 10
     // })
     // console.log(a)
+
+    // evalute --- single element 
+    // evalute --- multiple elements
+
+    // Changing UI for debug
+    //await page.locator('#name').evaluate(el=>el.style.border = "3px solid red")
+
+    // scroll element manually
+    await page.locator('#mousehover').evaluate(el=>el.scrollIntoView())
+
+    // Scenario 
+
+    // Need width/height           =====> evalute()
+    // Need custome style          =====> evalute()
+    // Need DOM    values          =====> evalute()
+    // Need many element texts     =====> evaluteAll()
+    // Need ids of all checkboxs   =====> evaluteAll()
+    // filer   -----> map() , filter() , reduce(), forEach(),every(),some(),find(),findIndex()
+
+    let prices = await page.locator('#product')
+    .first()
+    .locator('tr td:nth-child(3)')
+    .evaluateAll(els => els.map(el=>Number(el.textContent)))
+    console.log(prices)
+    let acc = prices.reduce(function(acc,el){
+        return el + acc
+    },0)
+    console.log(acc)
+    // assertion for total
+
+    let cities = await page.locator('#product')
+    .last()
+    .locator('tr td:nth-child(3)')
+    .evaluateAll(els => els.map(el=>el.textContent))
+    console.log(cities)
+    let result = cities.some((el)=>el == "Chennai")
+    console.log(result)
+    //await expect(result).toBe(true)
+    
+    // Test ---> Arrangement ---> action and assertion
+
+    let citiesE = await page.locator('#product')
+    .last()
+    .locator('tr td:nth-child(3)') // [el,el,el,el] ==> [el,el]
+    .evaluateAll(els => els.filter(el=>el.textContent.trim() == "Chennai"))
+    console.log(citiesE)
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
