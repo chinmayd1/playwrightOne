a17fed1769e272dc3f50438700aa788e65da5beb
chinmayd1 <chinmay.minskole@gmail.com>
2026-05-13 21:34:18 +0530

    added

--- tests/fixtureone.spec.js
+++ tests/fixtureone.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('check title for page', async ({ page,browser }) => {
+test('check title for page', async ({ page }) => {
     // Arrangement
     await page.goto("https://playwright.dev/")
     // Assertion
@@ -10,9 +10,9 @@ test('check title for page', async ({ page,browser }) => {
 
     // if page is not used as fixture ---->
     // launch the browser
-    // creates context 
-    // open the tab
-    // give tab to your test
+    // creates context --- local storage , cookies ,session
+    // open the tab  --  opening the tab
+    // give tab to your test --> test ---> page
 
     // const browser= await chromium.launch();
     // const context = await browser.newContext();
@@ -51,11 +51,62 @@ test('check title for page', async ({ page,browser }) => {
     // Edge
     // Firefox
 
-    
-
 
 
 
 
 
 })
+
+test('check title for broswer fixture', async ({ browser }) => {
+    // broswer   
+    // context 
+    // page
+    let page1 = await browser.newPage()
+    let page2 = await broswer.newPage()
+    page1.goto('https://www.google.com') // admin
+    page2.goto('https://www.facebook.com') // user
+})
+
+
+test('check title for broswer fixture', async ({ browser }) => {
+    // broswer   
+    // context 
+    // page
+    let page1 = await browser.newPage()
+    let page2 = await broswer.newPage()
+    page1.goto('https://www.google.com') // admin
+    page2.goto('https://www.facebook.com') // user
+})
+
+// browser  ---> page1 and page2
+// browser ---> context ---> browser -user1   ---> browser --- user2
+// cookies ---> local storage ---> sessions ---> authentication
+
+
+//  userid password ----> authenticate ----> token -----> localstorage  / cookies
+
+
+// Broswe fixture and methods 
+
+
+// Context 
+test('check title for broswer fixture', async ({ browser }) => {
+    // broswer   
+    // context 
+    // page
+    let mode1 = await browser.newContext()
+    let mode1 = await broswer.newContext()
+    
+    let page1 = mode1.newPage()
+    let page2 = mode2.newPage()
+
+    //cookies ---> local storage ---> sessions ---> authentication
+
+    //Browser ---> Context ----> Page
+
+
+})
+
+
+// custiomized fixture
